3530ee7cc058d2394030ec655321afba55adf6d2
hnaq <quanganhhipboy@gmail.com>
2026-02-02 14:02:13 +0700

    giao diện quản lý danh mục món ăn admin

---
+++ admin/manage-category.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quản lý danh mục</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Quản lý danh mục</h1>
+
+        <br><br>
+
+        <a href="" class="btn-primary">Thêm danh mục</a>
+
+        <br><br><br>
+
+        <table class="tbl-full">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Tên danh mục</th>
+                    <th>Hình ảnh</th>
+                    <th>Nổi bật</th>
+                    <th>Hoạt động</th>
+                    <th>Thao tác</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Bánh mì</td>
+                    <td>
+                        <img src="../image/category/Food_Category_65.jpg" width="100px" alt="Bánh mì">
+                    </td>
+                    <td>Yes</td>
+                    <td>Yes</td>
+                    <td>
+                        <a href="" class="btn-secondary">Cập nhật</a>
+                        <a href="" class="btn-danger">Xóa</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>2</td>
+                    <td>Xúc Xích</td>
+                    <td>
+                        <img src="../image/category/Food_Category_296.jpg" width="100px" alt="Xúc Xích">
+                    </td>
+                    <td>No</td>
+                    <td>Yes</td>
+                    <td>
+                        <a href="" class="btn-secondary">Cập nhật</a>
+                        <a href="" class="btn-danger">Xóa</a>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>Cơm rang</td>
+                    <td>
+                        <img src="../image/category/Food_Category_157.avif" width="100px" alt="Cơm rang">
+                    </td>
+                    <td>Yes</td>
+                    <td>No</td>
+                    <td>
+                        <a href="" class="btn-secondary">Cập nhật</a>
+                        <a href="" class="btn-danger">Xóa</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<div class="footer">
+    <div class="wrapper">
+        Bản quyền &copy; 2026 WowFood
+    </div>
+</div>
+
+</body>
+</html>
