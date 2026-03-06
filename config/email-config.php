06a46608ae929802284f75f989ee7e7d8964c3ca
ducluong1003 <Anhductam123@gmail.com>
2026-03-06 14:21:34 +0700

    giao diện quản lý món ăn

--- config/email-config.php
+++ config/email-config.php
@@ -12,10 +12,7 @@ define('SMTP_PASSWORD', 'zddfjimwdngfougb'); // App Password (16 ký tự, KHÔN
 define('SMTP_FROM_EMAIL', 'wowfood6868@gmail.com'); // Email gửi đi
 define('SMTP_FROM_NAME', 'WowFood');
 
-// Lưu ý: 
-// 1. Cần bật "Less secure app access" hoặc tạo "App Password" trong Gmail
-// 2. App Password: https://myaccount.google.com/apppasswords
-// 3. Nếu dùng 2FA, bắt buộc phải dùng App Password
+
 
 ?>
 
